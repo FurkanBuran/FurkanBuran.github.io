a73811e9f08842e76e8eb33fce76fd59e9e26b1f
Furkan Buran <73740727+FurkanBuran@users.noreply.github.com>
2020-12-31 17:53:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,225 +1,266 @@
+
 <!DOCTYPE html>
 <html>
-<!----------------------------------------------------------------------------------------------------------------------------------------->
 <head>
 	<meta charset="utf-8">
-	<title>Furkan Buran | GitHub FurkanBuran</title> <!--Site ismi-->
-
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"></head>
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<!----------------------------------------------------------------------------------------------------------------------------------------->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HACKED BY SİPAHİ HACK TEAM</title>
+	<meta name="Abstract" content="Hacked By Sipahi Hack Team - Turkish Hacker Team">
+	<meta name="description" content="Turkish Hacker Team">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.17.0/TweenMax.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200&display=swap" rel="stylesheet">
 </head>
-<body onLoad="openPeeker()">  
-<!---------------------HESAP MAKİNESİ---------------------->
-<center> <h1 class="display-1">Hesap Makinesi <br> <h3>Tasarım: <a href="https://www.youtube.com/channel/UCeQuTVNcRaE3h1tyfQp-pYg?sub_confirmation=1">FurkanBuran/YouTube</a></h3> </h1> </center>  
+<body> <div class="option">
 
-
-<div class="container p-3 my-3 bg-dark text-white" style="border-style: double;">
-    <input style="font-family: Franklin Gothic Heavy; background-color: #F0F8FF;" type="number" name="" class="form-control" placeholder="1.sayı" onkeyup="hesapla()" id="sayi1">
-    <br>
-    <input style="font-family: Franklin Gothic Heavy; background-color: #F0F8FF;" type="number" name="" class="form-control" placeholder="2.sayı" onkeyup="hesapla()" id="sayi2">
-</div>
-
-
-
-<div class="container p-3 my-3 bg-dark text-white" style="font-family: Franklin Gothic Heavy; border-style: double;">
-  <div class="row">
-            <div class="col-lg-3">
-              <label for="">Toplam</label>
-              <input class="form-control" name="" id="toplam">
-            </div>
-
-            <div class="col-lg-3">
-              <label for="">Fark</label>
-              <input class="form-control" name="" id="fark">
-            </div>
-
-            <div class="col-lg-3">
-              <label for="">Çarpım</label>
-              <input class="form-control" name="" id="carpim">
-            </div>
-
-            <div class="col-lg-3">
-              <label for="">Bölümü</label>
-              <input class="form-control" name="" id="bolum">
-            </div>
-
-            <div class="col-lg-3">
-              <label for="">Yüzde</label>
-              <input class="form-control" name="" id="yüzde">
-            </div>   
-  </div>      
-</div>
-
-
-
-
-  
-      <script src="js/jquery.js" type="text/javascript"></script>
-      <script src="js/bootstrap.js" type="text/javascript"></script>
-
-<script>
-  function hesapla (){
-    var sayi1= Number($("#sayi1").val());
-    var sayi2= Number($("#sayi2").val());
-
-
-  var toplam =sayi1+sayi2;
-  var carpim =sayi1*sayi2;
-  var fark =sayi1-sayi2;
-  var bolum =sayi1/sayi2;
-  var yüzde =sayi1%sayi2;
-
-  $("#toplam").val(toplam);
-   $("#carpim").val(carpim);
-    $("#fark").val(fark);
-     $("#bolum").val(bolum);
-      $("#yüzde").val(yüzde);
-
-  console.log(sayi1+""+sayi2)
-
+<script type="text/javascript">
+function IE(e) {
+     if (navigator.appName == "Microsoft Internet Explorer" && (event.button == "2" || event.button == "3")) {
+          alert('UYARI');
+          return false;
+     }
 }
+function NS(e) {
+     if (document.layers || (document.getElementById && !document.all)) {
+          if (e.which == "2" || e.which == "3") {
+               alert('Kodları kopyalamak yasaktır! (Sipahi Hack Team)');
+               return false;
+          }
+     }
+}
+document.onmousedown=IE;document.onmouseup=NS;document.oncontextmenu=new Function("return false");
 </script>
 
-<!----------------------END----------------------------->
+<!--Değişen Start--><script LANGUAGE='JavaScript'>
 
+var default1 = "Hacked By Sipahi Hack Team"; 
+var text1 = "Sipahi Hack Team"; 
+var text2 = "Hacked By"; 
+var text3 = "Sipahi Hack Team"; 
+var text4 = "Hacked By Sipahi Hack Team"; 
+var changeRate = 1000; 
+var messageNumber = 0;
 
-
-<!----------------------POP-UP----------------------------->
-<SCRIPT LANGUAGE="JavaScript">
-
-
-<!-- Begin
-// burası en ve yüksekliğini ayarlayın
-
-var windowW = 1280;  // en
-var windowH = 720;  // yükseklik
-
-
-
-var Yoffset = 0;   
-
-
-var windowStep = 2;
-var moveSpeed = 10; 
-
-
-Xmode = "right";
-
-
-Xoffset = 35;
-
-// Yüklenmesini istediğiniz sayfanın URLsi
-
-var urlPop = "https://www.youtube.com/channel/UCeQuTVNcRaE3h1tyfQp-pYg?sub_confirmation=1"
-
-// sayfanın title'i
-
-var title =  "MuhammedFurkan-YouTube";
-
-var autoclose = true;
-
-
-// Buradan aşağısını değiştirmeyin
-
-
-var windowX = (screen.width/2)-(windowW/2);
-if (Xmode=="left") windowX=0+Xoffset;
-if (Xmode=="right") windowX=screen.availWidth-Xoffset-windowW;
-var windowY = (screen.availHeight);
-var windowYstop = windowY-windowH-Yoffset;
-var windowYnow = windowY;
-
-s = "width="+windowW+",height="+windowH;
-var beIE = document.all?true:false
-function openPeeker() {
-if (beIE) {
-PFW = window.open("","popFrameless","fullscreen,"+s);
-PFW.blur();
-window.focus();
-PFW.resizeTo(windowW,windowH);
-PFW.moveTo(windowX,windowY);
-var frameString=""+
-"<html>"+
-"<head>"+
-"<title>"+title+"</title>"+
-"</head>"+
-"<frameset rows='*,0' framespacing=0 border=0 frameborder=0>"+
-"<frame name='top' src='"+urlPop+"' scrolling=auto>"+
-"<frame name='bottom' src='about:blank' scrolling='no'>"+
-"</frameset>"+
-"</html>"
-PFW.document.open();
-PFW.document.write(frameString);
-PFW.document.close();
+function changeStatus() {
+if (messageNumber == 0) {
+window.status=default1;
+document.title=default1;
 }
-else {
-PFW=window.open(urlPop,"popFrameless","scrollbars,"+s);
-PFW.blur();
-window.focus(); 
-PFW.resizeTo(windowW,windowH);
-PFW.moveTo(windowX,windowY);
+else if (messageNumber == 1) {
+window.status=text1;
+document.title=text1;
 }
-PFW.focus();
-if (autoclose) {
-window.onunload = function(){PFW.close()}
+else if (messageNumber == 2) {
+window.status=text2;
+document.title=text2;
 }
-movePFW();
+else if (messageNumber == 3) {
+window.status=text3;
+document.title=text3;
 }
-function movePFW() {
-if (document.all) {
-if (windowYnow>=windowYstop) {
-PFW.moveTo(windowX,windowYnow);
-PFW.focus();
-windowYnow=windowYnow-windowStep;
-timer=setTimeout("movePFW()",moveSpeed);
+else if (messageNumber == 4) {
+window.status=text4;
+document.title=text4;
+messageNumber = 0;
 }
-else {
-clearTimeout(timer);
-PFW.moveTo(windowX,windowYstop);
-PFW.focus();
-   }
-}
-else {
-PFW.moveTo(windowX,windowYstop);
-   }
-}
-</script>
 
-<!-- End -->
-<div class="container p-3 my-3" style="background-image: url('https://www.imagevisit.com/images/2020/06/23/Fatih-Sultan-Mehmet-Han.jpg');">
+messageNumber++;
+setTimeout("changeStatus();",changeRate);
 
-<center>
-  <div class="display-1" style="font-size: 150%; border-style: double;">Fatih Sultan Mehmet</div>
-</center>
+}
 
-<center>
-  <p class="display-1" style="font-size: 150%; margin-top: 20px;">İmkanın sınırını görmek için imkansızı denemek lazım.</p>
-</center>
+changeStatus();
+</SCRIPT>
+<!--Değişen Stop-->
+
+<style type="text/css">
+	.option {
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	overflow: hidden;
+	}
+
+	body{
+		background-color: black;
+	}
+</style>
 
 
+<!-- Particless -->
+	<div id="particles-js"></div> 
+	<div class="count-particles"> 
+	<script src="http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+
+	<style type="text/css">
+		/* ---- reset ---- */
+canvas {
+  display: block;
+  vertical-align: bottom;
+} /* ---- particles.js container ---- */
+#particles-js {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  /* background-color: ; 
+  background-image: url(""); */
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 50%;
+  z-index: 3;
+} /* ---- stats.js ---- */
+	</style> 
+
+	<script type="text/javascript">
+		particlesJS("particles-js", {
+  particles: {
+    number: { value: 71, density: { enable: false, value_area: 0 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 3 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 0.2762016745712954,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+    },
+    line_linked: {
+      enable: false,
+      distance: 299.87610382026367,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+
+	</script>
+<!-- Particless -->
+
+<!-- Yazı ve Logo -->
+<div class="container-mid" style="text-align: center; margin-top: 200px;">
+	<img src="https://www.imagevisit.com/images/2020/08/09/Logo-Son.png" class="logo-sht" width="200" style="z-index: 2;">
+
+<!-- Yazı ve Logo Exit -->
+<br><br>
+
+<div class="text-home">
+    <div style="position: relative; height: 68px;">
+
+          <div class="home" style="font-family: 'Raleway', sans-serif; letter-spacing: 5px;">
+            <h2>SİPAHİ HACK TEAM - TURKİSH CYBER ARMY</h2>
+          </div>
+
+          <div class="h2-text" style="font-family: 'Raleway', sans-serif; letter-spacing: 5px;">
+            <h2>HELLO ADMİN SYSTEM HACKED!!!</h2>
+          </div>
+    </div>
+</div>
+      <br><br><br>
+      <p style="color: white; font-family: 'Raleway', sans-serif; letter-spacing: 5px;">ATABEY | TEOMAN | METE HAN</p>
 </div>
 
+<style type="text/css">
+.home {
+    width: 100%;
+    margin: 0 auto;
+    text-align: center;
+}
 
+.text-home {
+	color: white;
+}
+</style>
 
+<!-- ARKA PLAN -->
+	<style type="text/css">
+		.background {
+		position: absolute;
+    	top: -3%;
+    	left: -10%;
+    	width: 120%;
+    	height: 120%;
+    	z-index: 1;
+    	opacity: 1;
+		}
+	</style>
 
+	<script type="text/javascript">
+		$.fn.parallax = function ( resistance, mouse ) 
+{
+	$el = $( this );
+	TweenLite.to( $el, 0.2, 
+	{
+		x : -(( mouse.clientX - (window.innerWidth/2) ) / resistance ),
+		y : -(( mouse.clientY - (window.innerHeight/2) ) / resistance )
+	});
 
+};
+$( document ).mousemove( function( e ) {
+			$( '.background' ).parallax( 30, e );
+		});
+	</script>
+<div class="img-body1">
+	<img src="https://www.technopat.net/sosyal/eklenti/bayrak-1-jpg.19223/" class="background" style="opacity: 0.11;">
+</div>
+<!-- ARKA PLAN EXİT -->
 
-
-
-
-
-
-
-
-
-
+</div>
 
 </body>
 </html>
