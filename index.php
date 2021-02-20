256c816885b1c755559a4f6be0192c2beafccb5d
Furkan Buran <73740727+FurkanBuran@users.noreply.github.com>
2021-02-20 12:21:25 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <body cz-shortcut-listen="true">
+
+<div class="movie-card" style="background-image: url(&quot;https://i4.hurimg.com/i/hurriyet/75/1200x675/5bd0612ad3806c2870f2cf0a.jpg&quot;);">
+  <h1>TRT 1</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>TRT 1, Türkiye'nin ülke çapında yayın yapan ilk ulusal televizyon kanalıdır. Ana binası Ankara'dadır. Türkiye'de açılan ikinci televizyon kanalı ve ilk ulusal televizyon kanalıdır. İlk genel müdürü 29 Nisan 1964'te Adnan Öztrak olmuştur. İlk yayını Nuran Devres spikerliğinde Ankara'da gerçekleştirildi.</p><button class="watch"><a href="kanallar/trt1.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://foto.haberler.com/haber/2021/02/15/15-subat-2021-atv-yayin-akisi-13927062_amp.jpg&quot;);">
+  <h1>ATV</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>atv, Turkuvaz Medya Grubu bünyesinde Türkiye'de yayın yapan ulusal televizyon kanallarından biridir. Tüm Türkiye'ye yayın yapan ulusal kanal, genelde dizi filmleriyle ön plana çıkmıştır. Kanalın genel yayın yönetmeni Metin Ergen'dir.</p><button class="watch"><a href="404.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://i.kanald.com.tr/i/kanald/70/1200x630/5b8c55b5e94fb71488087b88.jpg&quot;);">
+  <h1>KANAL D</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>Kanal D, Demirören Holding'in Türkiye'de yayın yapan ulusal televizyon kanallarından biridir. Tüm Türkiye'ye yayın yapan ulusal kanal, genelde dizi filmleriyle ön plana çıkmıştır.</p><button class="watch"><a href="kanallar/kanald.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://i4.hurimg.com/i/hurriyet/75/750x422/568e15bb67b0a91c1c8e9a37.jpg&quot;);">
+  <h1>TV8</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>TV8, 22 Şubat 1999 tarihinde MNG Medya Grubu bünyesinde yayına başlayan, 2013 yılından bu yana Acun Medya bünyesinde yayın yapan bir televizyon kanalıdır. Merkez stüdyoları Sarıyer'de bulunmaktadır.</p><button class="watch"><a href="kanallar/tv8.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://trthaberstatic.cdn.wp.trt.com.tr/static/images/trt-haber-share-logo.png&quot;);">
+  <h1>TRT HABER</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>TRT Haber, TRT tarafından 18 Mart 2010 tarihinde TRT 2'nin önceki frekansında yayına başlamasıyla kurulan haber kanalıdır. 18 Kasım 2013 tarihinde logosunu ve stüdyolarını değiştirip eş zamanlı HD Yayına geçmiştir.</p><button class="watch"><a href="kanallar/trthaber.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://i.pinimg.com/originals/1b/d7/3e/1bd73eb4e6449bd6bcebd129b1dbac5a.jpg&quot;);">
+  <h1>TGRT HABER</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>TGRT Haber, İhlas Holding bünyesinde bulunan bir haber kanalı. 29 Ekim 2004 tarihinde yayın hayatına başlamıştır. TGRT'nin Amerikalı News Corporation şirketine satılmasına karşın kanal, kardeş kanalları TGRT Pazarlama ve TGRT FM ile birlikte İhlas Holding bünyesinde yayın yapmaktadır.</p><button class="watch"><a href="kanallar/tgrthaber.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://cdn1.startv.com.tr/media/News/_ImageOriginal/830fdb9c-1f28-43be-993f-dfa080665c66.jpg?v=43943,7502622338&quot;);">
+  <h1>STAR TV</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>Star TV, Doğuş Yayın Grubu bünyesinde Türkiye'de yayın yapan ulusal televizyon kanalıdır. Star TV'nin genel yayın yönetmeni Nermin Yurteri'dir.</p><button class="watch"><a href="kanallar/startv.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://mo.ciner.com.tr/video/2020/12/25/ver1608921001/60785E0F11E0FDE6FD6357361C44003F_640x360.jpg&quot;);">
+  <h1>SHOW TV</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>Show TV, Ciner Yayın Holding bünyesinde yayın yapan ulusal televizyon kanalıdır. Show TV'nin genel müdürü Esra Baydar'dır.</p><button class="watch"><a href="kanallar/showtv.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://yt3.ggpht.com/ytc/AAUvwnjH_FNXdUCkqQimQBUb2Ta5DKXaiyfpFL2rpeszsz0=s900-c-k-c0x00ffffff-no-rj&quot;);">
+  <h1>NTV</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>NTV, 1996 yılında Cavit Çağlar tarafından kurulan, Türkiye'nin ilk haber kanalıdır. Ocak 1999'da Doğuş Yayın Grubu bünyesine katılarak yakaladığı başarı ile Türk medya endüstrisini değiştiren NTV, Türkiye'de tematik kanal dönemini başlattı.</p><button class="watch"><a href="kanallar/ntv.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://izle7-image.kanal7-cdn.com/assets/img/kanal7-logo_new.jpg&quot;);">
+  <h1>KANAL 7</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>Kanal 7, Yeni Dünya Medya Grubu bünyesinde Türkiye'de yayın yapan ulusal televizyon kanalıdır. Kanal 7'nin genel yayın yönetmeni Zahid Akman'dır.</p><button class="watch"><a href="kanallar/kanal7.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://pbs.twimg.com/profile_images/1234192640318595073/l3AhSXz7_400x400.jpg&quot;);">
+  <h1>HABERTURK</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>Habertürk, Ciner Yayın Holding bünyesinde 1 Mart 2009 tarihinde yayın hayatına başlayan günlük gazeteydi. Son sayısı 5 Temmuz 2018'de çıktı.</p><button class="watch"><a href="kanallar/haberturk.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://yt3.ggpht.com/ytc/AAUvwngUxKLHQsiJNqlgu9j86C4VR-vAqhd7_BEnwW9cXP8=s900-c-k-c0x00ffffff-no-rj&quot;);">
+  <h1>CNN Türk</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>CNN Türk, Amerikalı CNN şirketi ve Demirören Holding bünyesinde yayın yapan haber kanalı. Merkezi İstanbul'da olan kanalın en bilinen programları arasında 5N1K, Bir Yudum İnsan ve Oradaydım gibi yapımlar yer alır.</p><button class="watch"><a href="kanallar/cnnturk.php" target="_blank">CANLI İZLE</a></button>
+</div>
+<div class="movie-card" style="background-image: url(&quot;https://s1.dmcdn.net/v/GVBh61VaMqG0huAIL/x1080&quot;);">
+  <h1>BEYAZ TV</h1><span>60 min</span><span>5 <i class="fas fa-star"></i></span>
+  <p>Beyaz TV, 24 Mayıs 2010'da kurulan ulusal televizyon kanalı.</p><button class="watch"><a href="kanallar/beyaztv.php" target="_blank">CANLI İZLE</a></button>
+</div>
+
+</body>
+</html>
+
